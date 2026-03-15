921b0d205d2318c93ee3209a118befde6281bf72
kyle <kyle@lindsay.gr>
2026-03-15 16:41:38 +0000

    css updates

--- .next/dev/static/chunks/app_page_tsx_75f114db._.js
+++ .next/dev/static/chunks/app_page_tsx_75f114db._.js
@@ -1,8 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/node_modules_next_dist_02409dcd._.js",
-  "static/chunks/app_ui_92cbfbd8.css"
+  "static/chunks/components_2751b8f4._.css",
+  "static/chunks/node_modules_next_dist_60d9f176._.js"
 ],
     source: "dynamic"
 });
